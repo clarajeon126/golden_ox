2d01ac9deebff52bd3b325ed03e98c9910bc5a9e
Jeremy Ko <jko26@Jeremys-MacBook-Air-2.local>
2025-04-27 12:05:55 -0400

    button

--- app/simulation/components/NavigationArrows.jsx
+++ app/simulation/components/NavigationArrows.jsx
@@ -1,10 +1,20 @@
-// /components/NavigationArrows.jsx
+"use client"; // ✅ because we are using hooks
+
+import { useRouter } from "next/navigation";
 import styles from "../styles/NavigationArrows.module.css";
 
 export default function NavigationArrows() {
+  const router = useRouter();
+
+  function handleBack() {
+    router.push("/start"); // 🚀 Go back to Start Page
+  }
+
   return (
     <div className={styles.navArrows}>
-      <button className={styles.arrow}>back</button>
+      <button className={styles.arrow} onClick={handleBack}>
+        back
+      </button>
     </div>
   );
 }
--- app/simulation/components/Visual.jsx
+++ app/simulation/components/Visual.jsx
@@ -2,6 +2,10 @@
 import { useSimulation } from "../context/SimulationContext";
 import styles from "../styles/Visual.module.css";
 
+const res = await fetch("/parsed_pcr.json", { cache: "no-store" });
+const scenarioData = await res.json();
+const narrative_summary = scenarioData.narrative_summary;
+
 export default function Visual() {
   const { visual } = useSimulation();
 
